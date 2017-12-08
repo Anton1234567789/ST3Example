af18bf6296067c991e9cedc0f12bf2706a053621
AntonSokolov <toshiksokil@mail.ru>
2017-12-08 17:38:04 +0200

    Initial commit

---
+++ src/ua/nure/your_last_name/SummaryTask3/entity/Answer.java
@@ -0,0 +1,32 @@
+package ua.nure.your_last_name.SummaryTask3.entity;
+
+public class Answer {
+
+	private String content;
+
+	private Boolean correct;
+
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String value) {
+		this.content = value;
+	}
+
+	public boolean isCorrect() {
+		if (correct == null) { 
+			return false;
+		}
+		return correct;
+	}
+
+	public void setCorrect(Boolean value) {
+		this.correct = value;
+	}
+	
+	@Override
+	public String toString() {
+		return content + (isCorrect() ? " [correct=true]" : "");		
+	}
+}
